5ce18c9b4083312d2ebbdfe8c4b3be902e31b10a
Eric Hanson <ehanson@mit.edu>
2019-12-18 17:17:48 -0500

    Create constant classes

---
+++ src/main/java/edu/mit/dos/model/ContentSystems.java
@@ -0,0 +1,7 @@
+package edu.mit.dos.model;
+
+public final class ContentSystems{
+  public static final String Dome = "c1";
+  public static final String DSpace = "c2";
+  public static final String DIPOnline = "c3";
+}
---
+++ src/main/java/edu/mit/dos/model/MetadataSystems.java
@@ -0,0 +1,6 @@
+package edu.mit.dos.model;
+
+public final class MetadataSystems{
+  public static final String ArchivesSpace = "m1";
+  public static final String Aleph = "m2";
+}
