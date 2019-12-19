21aa0255345f9a474cfcb7a45f800ec0b7823026
Eric Hanson <ehanson@mit.edu>
2019-12-19 13:36:04 -0500

    Class merge

--- src/main/java/edu/mit/dos/model/ContentSystems.java
+++
@@ -1,7 +0,0 @@
-package edu.mit.dos.model;
-
-public final class ContentSystems{
-  public static final String DOME = "c1";
-  public static final String DSPACE = "c2";
-  public static final String DIPONLINE = "c3";
-}
--- src/main/java/edu/mit/dos/model/MetadataSystems.java
+++
@@ -1,6 +0,0 @@
-package edu.mit.dos.model;
-
-public final class MetadataSystems{
-  public static final String ARCHIVESSPACE = "m1";
-  public static final String ALEPH = "m2";
-}
