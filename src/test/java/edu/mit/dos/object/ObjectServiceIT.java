54c78d0ef9ca68f5f8721362040c098ebea067b3
Eric Hanson <ehanson@mit.edu>
2020-02-17 11:53:12 -0500

    misc edits and property name changes

--- src/test/java/edu/mit/dos/object/ObjectServiceIT.java
+++ src/test/java/edu/mit/dos/object/ObjectServiceIT.java
@@ -142,8 +142,8 @@ public class ObjectServiceIT {
         updatedMap.add("oid", oid); // the object id to update
         updatedMap.add("handle", "test.update.hdl.net");
         updatedMap.add("title", "Item Title Updated");
-        updatedMap.add("metadata_system", "dome");
-        updatedMap.add("source_system", "archivesspace");
+        updatedMap.add("metadata_source", "dome");
+        updatedMap.add("content_source", "archivesspace");
         updatedMap.add("target_links", "https://dome.mit.edu/bitstream/handle/1721.3/21882/112045_sv.jpg?sequence=2");
         final HttpEntity<MultiValueMap<String, String>> updateRequest = new HttpEntity<>(updatedMap, new HttpHeaders());
         final String body2 = this.restTemplate.patchForObject("/object", updateRequest, String.class);
@@ -160,8 +160,8 @@ public class ObjectServiceIT {
         final MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("handle", "test.post.hdl.net");
         map.add("title", "Item Title");
-        map.add("metadata_system", "dome");
-        map.add("source_system", "archivesspace");
+        map.add("metadata_source", "dome");
+        map.add("content_source", "archivesspace");
         map.add("target_links", "https://dome.mit.edu/bitstream/handle/1721.3/176391/249794_cp.jpg?sequence=1");
         final HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, new HttpHeaders());
         final String oid = this.restTemplate.postForObject("/object", request, String.class);
@@ -174,8 +174,8 @@ public class ObjectServiceIT {
         updatedMap.add("oid", oid); // the object id to update
         updatedMap.add("handle", "test.update.hdl.net");
         updatedMap.add("title", "");
-        updatedMap.add("metadata_system", "dome");
-        updatedMap.add("source_system", "archivesspace");
+        updatedMap.add("metadata_source", "dome");
+        updatedMap.add("content_source", "archivesspace");
         updatedMap.add("target_links", "https://dome.mit.edu/bitstream/handle/1721.3/21882/112045_sv.jpg?sequence=2");
         final HttpEntity<MultiValueMap<String, String>> updateRequest = new HttpEntity<>(updatedMap, new HttpHeaders());
         final String body2 = this.restTemplate.patchForObject("/object", updateRequest, String.class);
@@ -221,8 +221,8 @@ public class ObjectServiceIT {
         final MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("handle", "hdl.net");
         map.add("title", "Item Title");
-        map.add("metadata_system", "dome");
-        map.add("source_system", "archivesspace");
+        map.add("metadata_source", "dome");
+        map.add("content_source", "archivesspace");
         map.add("target_links", "https://dome.mit.edu/bitstream/handle/1721.3/46021/MC665_r14_6M-4371.pdf?sequence=1");
         return map;
     }
